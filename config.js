11abad23e009385a09ea410d186ad31f3ca5fb9d
odos <odos2005@gmail.com>
2023-06-05 11:49:24 -0300

    first upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    color_hsl: [228,82],
+    color_step: -30,
+    color_start: 200
+}
---
+++ main.js
@@ -0,0 +1,53 @@
+// COLORED TITLE
+for (let i=0;i<document.getElementById("grid_container").children.length;i++) {
+    let j = document.getElementById("grid_container").children.item(i)
+    j.children.item(0).style.color = "hsl("+((i*config.color_step)+config.color_start)+","+config.color_hsl.join("%,")+"%)"
+    console.log(j.children.item(0))
+}
+
+// document.querySelectorAll("a").forEach(el => {
+//     el.setAttribute("target", "_blank")
+// })
+// const searchInput = document.querySelector('input[name="q"]');
+// const originalPlaceholder = searchInput.placeholder;
+
+// document.addEventListener('keydown', function(event) {
+//   if (event.code === 'Escape') {
+//     event.preventDefault();
+//     searchInput.value = '';
+//     searchInput.placeholder = originalPlaceholder;
+//   }
+// });
+
+
+
+
+const searchInput = document.querySelector('input[type="text"]');
+const placeholderText = searchInput.getAttribute('placeholder');
+
+searchInput.addEventListener('focus', function() {
+  if (this.value === placeholderText) {
+    this.value = '';
+  }
+});
+
+searchInput.addEventListener('blur', function() {
+  if (this.value === '') {
+    this.value = placeholderText;
+  }
+});
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape') {
+    searchInput.value = placeholderText;
+  }
+});
+
+window.onload = function() {
+  var images = ["img/bg.png", "img/bg2.png", "img/bg3.png", "img/bg4.png", "img/bg5.png", "img/wall.jpg", "img/denim.png"];
+  var rand = Math.floor(Math.random() * images.length);
+  var url = "url('" + images[rand] + "')";
+  document.documentElement.style.setProperty("--bg-image", url);
+};
+
+
